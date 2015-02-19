795c510b46fe3400323c5ace392e7e196cd5156c
TobiasMeister98 <meister.tobias21@gmail.com>
2015-02-19 01:17:47 +0100

    Added a page to create accounts

---
+++ tmp_register.php
@@ -0,0 +1,61 @@
+<?php
+
+if (isset($_SESSION["isLoggedIn"])) {
+    exit;
+}
+
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $username_form = $_POST["username"];
+    $password_form = $_POST["password"];
+    
+    $sha_pass = sha1($password_form);
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "DvsK*12G";
+    $dbname = "muemlingfischer";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) die("Connection failed: ".$conn->connect_error);
+    
+    $sql = "INSERT INTO login (username, password) VALUES ('$username_form', HEX('$sha_pass')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+}
+
+?>
+
+<h1>Login</h1>
+
+<br>
+
+<div class="login-main">
+    <form name="login-form" method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
+        <table>
+            <tr>
+                <td>Benutzername:</td>
+                <td><input type="text" name="username" maxlength="50"></td>
+            </tr>
+            <tr>
+                <td>Passwort:</td>
+                <td><input type="password" name="password"></input></td>
+            </tr>
+            
+            <tr class="table-spacer"></tr>
+            
+            <tr>
+                <td />
+                <td style="text-align: right">
+                    <input type="submit" value="Einloggen">
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
