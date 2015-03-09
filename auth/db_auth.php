7b546e6455b67878a396efc99158f4f50adf98dd
TobiasMeister98 <meister.tobias21@gmail.com>
2015-03-09 08:03:34 +0100

    Fixed db_auth variables

--- auth/db_auth.php
+++ auth/db_auth.php
@@ -5,7 +5,7 @@ $db_username = "root";
 $db_password = "DvsK*12G";
 $db_name = "muemlingfischer";
 
-$conn = new mysqli($db_servername, $db_username, $db_password, $db_dbname);
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
 
 if ($conn->connect_error) die("Connection failed: ".$conn->connect_error);
 
